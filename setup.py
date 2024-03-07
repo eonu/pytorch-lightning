b664dd347a61d04f7dfc7c91fb517d526f460eb3
Edwin Onuonga <ed@hazy.com>
2024-03-07 15:22:17 +0400

    specify pytorch package name

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ from typing import Generator, Mapping, Optional
 import setuptools
 import setuptools.command.egg_info
 
-_PACKAGE_NAME = os.environ.get("PACKAGE_NAME")
+# _PACKAGE_NAME = os.environ.get("PACKAGE_NAME")
+_PACKAGE_NAME = os.environ.get("PACKAGE_NAME", "pytorch")  # @eonu
 _PACKAGE_MAPPING = {
     "lightning": "lightning",
     "pytorch": "pytorch_lightning",
@@ -142,7 +143,6 @@ if __name__ == "__main__":
                 f"Unexpected package name: {_PACKAGE_NAME}. Possible choices are: {list(_PACKAGE_MAPPING)}"
             )
         package_to_install = _PACKAGE_MAPPING.get(_PACKAGE_NAME, "lightning")
-        package_to_install = "pytorch_lightning"  # @eonu: override
         if package_to_install == "lightning":
             # merge all requirements files
             assistant._load_aggregate_requirements(_PATH_REQUIRE, _FREEZE_REQUIREMENTS)
@@ -163,7 +163,6 @@ if __name__ == "__main__":
     setup_module = _load_py_module(name=f"{package_to_install}_setup", location=pkg_setup)
     setup_args = setup_module._setup_args()
     is_main_pkg = package_to_install == "lightning"
-    is_main_pkg = True  # @eonu: override
     print(f"Installing as the main package: {is_main_pkg}")
     if is_source_install:
         # we are installing from source, set the correct manifest path
